ff30c509723d8d8947f2451e28fe378ae5547e34
Olaf Meneses <menesesolaf@gmail.com>
2023-12-11 20:26:01 +0100

    Mapa de prueba base implementado

---
+++ prueba.R
@@ -0,0 +1,13 @@
+library(readxl)
+library(sf)
+library(dplyr)
+library(leaflet)
+
+lineas <- read_sf("./data/lineas/lineas.shp")
+estaciones <- read_excel("./data/estaciones.xlsx")
+
+leaflet() %>%
+  addProviderTiles(provider = "CartoDB.PositronNoLabels") %>%  
+  addPolylines(data = lineas, color = ~color) %>% 
+  addCircleMarkers(data = estaciones, lat = ~lat, lng = ~lng,
+                   popup = ~nombre, color = "gray", fillColor = "white", fillOpacity = 1, radius = 6, weight = 2)
